40234c6c2179d44190449a8e112e5b41c5bb0078
Jared Scott <jared.scott@variable.team>
2023-06-07 23:28:36 +0800

    Make changes to prettier, add it to .eslintrc, update files

--- packages/typedserver/src/controllers/rest/HelloWorldController.integration.spec.ts
+++ packages/typedserver/src/controllers/rest/HelloWorldController.integration.spec.ts
@@ -1,25 +1,27 @@
-import { PlatformTest } from "@tsed/common";
-import SuperTest from "supertest";
-import { HelloWorldController } from "./HelloWorldController";
-import { Server } from "../../Server";
+import { PlatformTest } from '@tsed/common';
+import SuperTest from 'supertest';
+import { HelloWorldController } from './HelloWorldController';
+import { Server } from '../../Server';
 
-describe("HelloWorldController", () => {
+describe('HelloWorldController', () => {
   let request: SuperTest.SuperTest<SuperTest.Test>;
 
-  beforeEach(PlatformTest.bootstrap(Server, {
-    mount: {
-      "/": [HelloWorldController]
-    }
-  }));
+  beforeEach(
+    PlatformTest.bootstrap(Server, {
+      mount: {
+        '/': [HelloWorldController]
+      }
+    })
+  );
   beforeEach(() => {
     request = SuperTest(PlatformTest.callback());
   });
 
   afterEach(PlatformTest.reset);
 
-  it("should call GET /hello-world", async () => {
-     const response = await request.get("/hello-world").expect(200);
+  it('should call GET /hello-world', async () => {
+    const response = await request.get('/hello-world').expect(200);
 
-     expect(response.text).toEqual("hello");
+    expect(response.text).toEqual('hello');
   });
 });
